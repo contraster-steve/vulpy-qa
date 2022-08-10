1b99a847dce9f1ecee555b65ca8dfa149677c291
Steve R. Smith <83226400+contraster-steve@users.noreply.github.com>
2022-08-10 10:13:58 -0500

    Update wrapper.py

--- bad/wrapper.py
+++ bad/wrapper.py
@@ -1,7 +1,7 @@
 #wrapper.py
 
 #from contrast.wsgi import ContrastMiddleware
-from contrast.flask import ContrastMiddleware # Vuply uses Flask instead of pure wsgi
+from contrast.flask import ContrastMiddleware # Vulpy uses Flask instead of pure wsgi
 
 main_module_name = 'vulpy.app'
 main_function_name = 'main'
